8d9675e5b843085ad3ffbdbbd609dc568cb2088f
joing.yao <609311059@qq.com>
2022-04-27 22:19:27 +0800

    hotfix: 矫正脚本文件

--- .sh/pfsc.sh
+++ .sh/pfsc.sh
@@ -2,4 +2,4 @@
 docker rm -f pfsc
 docker image rm pfsc:1.0.0
 docker build -t pfsc:1.0.0 .
-docker run --name pfsc -p 8080:8080 -v /home/log/pfsc:/home/appLog -e TZ="Asia/Shanghai" -d pfsc:1.0.0
+docker run -d -p 8080:8080 -v /home/log/pfsc:/home/appLog --name pfsc pfsc:1.0.0
